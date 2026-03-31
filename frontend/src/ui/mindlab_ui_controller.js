5276884e9f7539f71b557ae88dc7582466d3407a
Your Name <you@example.com>
2026-03-31 09:21:52 -0400

    Create UI controller linking engine to renderer

---
+++ frontend/src/ui/mindlab_ui_controller.js
@@ -0,0 +1,7 @@
+﻿import { runEngineCycle } from "./mindlab_engine_bridge.js"
+import { renderPuzzle } from "./mindlab_renderer.js"
+
+export function runUI(result) {
+    const puzzle = runEngineCycle(result)
+    return renderPuzzle(puzzle)
+}
