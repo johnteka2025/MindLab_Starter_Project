2951823cabdde25d1e27c48a4358602f60970336
Your Name <you@example.com>
2026-03-31 09:28:33 -0400

    Add full game loop test

---
+++ frontend/src/ui/mindlab_game_test.js
@@ -0,0 +1,8 @@
+﻿import { runGame } from "./mindlab_game_loop.js"
+
+const inputs = ["correct", "wrong", "invalid"]
+
+inputs.forEach(i => {
+    const output = runGame(i)
+    console.log(output)
+})
