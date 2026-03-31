85e70d61a3a3b1afb739a39fcb23b2d933fd5c78
Your Name <you@example.com>
2026-03-31 10:03:51 -0400

    Add CommonJS score route

---
+++ backend/routes/score.js
@@ -0,0 +1,8 @@
+﻿module.exports = function registerScoreRoute(app) {
+    app.post("/score", function (req, res) {
+        res.json({
+            ok: true,
+            received: req.body || null
+        })
+    })
+}
