45c288c885aa1e66054c75009b8a0cebeea27880
Your Name <you@example.com>
2026-03-31 11:28:48 -0400

    Create browser UI app controller

---
+++ frontend/src/ui/mindlab_browser_app.js
@@ -0,0 +1,16 @@
+﻿import { runPersistentGame } from "./mindlab_persistent_game_runner.js"
+
+let history = []
+
+function render(html) {
+  const app = document.getElementById("app")
+  app.innerHTML = html
+}
+
+window.run = async function(answer) {
+  const output = await runPersistentGame(answer, history)
+  history = output.history
+  render(output.puzzleHtml)
+}
+
+render("<button onclick=\"run('correct')\">Correct</button><button onclick=\"run('wrong')\">Wrong</button>")
