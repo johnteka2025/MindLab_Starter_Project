d52582fca6d251407e13def065fec4188e826c0a
Your Name <you@example.com>
2026-05-24 17:10:38 -0400

    Force GitHub Pages frontend asset base path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,5 @@
-﻿import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      onwarn(warning, warn) {
-        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
-          return;
-        }
-
-        warn(warning);
-      },
-    },
-  },
+  base: '/MindLab_Starter_Project/'
 });
